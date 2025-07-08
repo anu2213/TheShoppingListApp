a3a1d09d71d1c967ee6c33df03495c059ae9ca03
Anushka Saha <sahaanushka84@gmail.com>
2025-07-09 01:31:21 +0530

    Initial commit

---
+++ app/src/main/java/com/anushka/myshoppinglistapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.anushka.myshoppinglistapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.anushka.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
+
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyShoppingListAppTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    ShoppingListApp()
+
+                }
+            }
+        }
+    }
+}
+
+
+
